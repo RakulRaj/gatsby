7711af635a8f76afe0df4f0503ed933149150c2b
govimu8 <muneeshkumar.g@ptpll736.payoda.net>
2021-06-16 22:17:08 +0530

    404 added

---
+++ src/pages/404.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class test extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Coming Soon</h1>
+      </div>
+    )
+  }
+}
